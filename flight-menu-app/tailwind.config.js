f6ce02265f1779ba53c710fdfb9cf74b7d4bd7d5
Ali GÖKÇEK <aligokcek1@gmail.com>
2025-03-09 16:56:47 +0300

    fix

--- flight-menu-app/tailwind.config.js
+++ flight-menu-app/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/data/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/store/**/*.{js,ts,jsx,tsx,mdx}',
+    'flight-menu-app/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
