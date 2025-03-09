7ca22513c42a71c0a6404143fd5539a4187290ff
Ali GÖKÇEK <aligokcek1@gmail.com>
2025-03-09 16:48:39 +0300

    css

--- flight-menu-app/tailwind.config.ts
+++ flight-menu-app/tailwind.config.ts
@@ -1,15 +1,16 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        'default': '#000000',
+      },
+    },
   },
   plugins: [],
 }
-export default config
